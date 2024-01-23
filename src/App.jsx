9d66a886b111b8d5a61a51036ff476a80ed7a95e
marq sz <marekszulc2020@gmail.com>
2024-01-23 10:30:45 +0100

    chore: set initially this app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+import TopNav from "./components/TopNav/TopNav";
+
+function App() {
+  return (
+    <>
+      <TopNav />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TopNav/TopNav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+/* import '/src/css/TopNav.css' */
+import "./TopNav.scss";
+
+export default function TopNav() {
+  return (
+    <div>
+      <p id="naa">TopNav</p>
+      <p className="pp">bufuheuferfiumefirg</p>
+    </div>
+  );
+}

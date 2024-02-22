fea3551cdeb423efad863dbd1e10cb005e27490e
marq sz <marekszulc2020@gmail.com>
2024-02-22 11:04:33 +0100

    feat: add mapping Card for test into YouMidghtLike component and also styled it

--- src/UI/Cards/CardYouMightLike.jsx
+++ src/UI/Cards/CardYouMightLike.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import "./CardYouMightLike.scss";
-import ImgCard from "../../assets/img/coversMini/albums/flashback_from_2001-mini.jpg";
-export default function CardYouMightLike() {
+export default function CardYouMightLike({
+  imgItem,
+  titleItem,
+  titleArtist,
+  priceItem,
+}) {
   return (
     <div className="card__youMightLike">
       <div className="div-top-of-card">
         <div className="div-img-card__youMightLike">
           <img
             className="img-card__youMightLike"
-            src={ImgCard}
-            alt="album-img"
+            src={imgItem}
+            alt={`${titleItem} By ${titleArtist}`}
           />
         </div>
       </div>
       <div className="div-bottom-of-card">
-        {/* div 2 titles */}
+        {/* div titles */}
         <div className="two-titles-card">
           <div className="div-title-card__youMightLike">
-            <h4 className="title-card__youMightLike">
-              Album Flashback From 2001
-            </h4>
+            <h4 className="title-card__youMightLike">{titleItem}</h4>
           </div>
           <div className="div-title-card__youMightLike">
-            <h5 className="titleArtist-card__youMightLike">El Double M</h5>
+            <h5 className="titleArtist-card__youMightLike">{titleArtist}</h5>
           </div>
         </div>
-        <h5 className="price-card__youMightLike">12.99€</h5>
+        <h5 className="price-card__youMightLike">From {priceItem}€</h5>
+        {/*  <h5 className="price-card__youMightLike old-price"> 12.99€ - 32.99€</h5> */}
       </div>
     </div>
   );
--- src/components/YouMightLike.jsx
+++ src/components/YouMightLike.jsx
@@ -2,35 +2,57 @@ import React from "react";
 import "./YouMightLike.scss";
 import CardYouMightLike from "../UI/Cards/CardYouMightLike";
 export default function YouMightLike() {
+  const itemsData = [
+    {
+      imgItem: "/src/assets/img/coversMini/albums/arena_of_autumnn_EP-mini.jpg",
+      titleItem: "Album Arena Of Autumnn EP",
+      titleArtist: "El Double M",
+      priceItem: "7.99",
+    },
+    {
+      imgItem: "/src/assets/img/coversMini/albums/flashback_from_2001-mini.jpg",
+      titleItem: "Album Flashback From 2001",
+      titleArtist: "El Double M",
+      priceItem: "11.99",
+    },
+    {
+      imgItem: "/src/assets/img/other-members/albums/rose-terrace.jpg",
+      titleItem: "Album Rose Terrace",
+      titleArtist: "Molly Granoli",
+      priceItem: "8.49",
+    },
+    {
+      imgItem:
+        "/src/assets/img/other-members/albums/into-to-different-dimension.jpg",
+      titleItem: "Album Into To Different Dimension",
+      titleArtist: "Molly Granoli",
+      priceItem: "8.99",
+    },
+    {
+      imgItem: "/src/assets/img/other-members/albums/lone-rider-album.jpg",
+      titleItem: "Album Lone Rider",
+      titleArtist: "Jackie Ras",
+      priceItem: "9.99",
+    },
+    {
+      imgItem: "/src/assets/img/single-collection-albums/dream-chaser-2.jpg",
+      titleItem: "Album Collectors Dreamchasers II",
+      titleArtist: "El Double M",
+      priceItem: "11.99",
+    },
+  ];
+
   return (
     <article className="you-might-like">
       <div className="div-youMightLike">
         <h4 className="h-youMightLike">You may also like</h4>
         <ul className="ul-list-youMightLike">
-          <li className="li-youMightLike">
-            <CardYouMightLike />
-          </li>
-          <li className="li-youMightLike">
-            <CardYouMightLike />
-          </li>
-          <li className="li-youMightLike">
-            <CardYouMightLike />
-          </li>
-          <li className="li-youMightLike">
-            <CardYouMightLike />
-          </li>
-          <li className="li-youMightLike">
-            <CardYouMightLike />
-          </li>
-          <li className="li-youMightLike">
-            <CardYouMightLike />
-          </li>
-          <li className="li-youMightLike">
-            <CardYouMightLike />
-          </li>
-          <li className="li-youMightLike">
-            <CardYouMightLike />
-          </li>
+          {/* maping all items from the array */}
+          {itemsData.map((item, index) => (
+            <li className="li-youMightLike" key={index}>
+              <CardYouMightLike {...item} />
+            </li>
+          ))}
         </ul>
       </div>
     </article>

edfc826c82ccb83e7f06688f0ca986e63d96f756
marq sz <marekszulc2020@gmail.com>
2024-03-25 11:51:39 +0100

    feat: add initially a user profile page

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,6 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+import "./UserProfile.scss";
 
-export default function UserProfile() {
+import BadgeTopOfAsideNav from "../UI/Badge/BadgeTopOfAsideNav.jsx";
+export default function UserPage() {
   return (
-    <div>UserProfile</div>
-  )
+    <aside className="aside-user-profile">
+      {/* bagde with name of page and x to return */}
+      <BadgeTopOfAsideNav nameOfNavPage={"Profile"} />
+      <hr className="user-profile-devider-separator" />
+      <div className="all-content-of-user-profile">
+        <div className="content-empty-user-profile">
+          <div className="all-text-user-profile">
+            <p className="p-text-user-profile">Are you already a member?</p>
+            <h3 className="h-user-profile">Sign in</h3>
+            <p className="p-text-user-profile">First time here?</p>
+            <h3 className="h-user-profile">Join Us</h3>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
 }

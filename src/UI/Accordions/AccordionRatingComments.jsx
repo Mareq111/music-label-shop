3a09cf527108decc26e9fa01c424873eccaab817
marq sz <marekszulc2020@gmail.com>
2024-05-07 11:55:10 +0200

    feat: add text when item has no comments and reviews into product details

--- src/UI/Accordions/AccordionRatingComments.jsx
+++ src/UI/Accordions/AccordionRatingComments.jsx
@@ -1,61 +1,5 @@
-/* 
-import RadioStarRating from "../Inputs/RadioStarRating.jsx";
-import TextAreaComment from "../Text/TextAreaComment.jsx";
-import TextAddedComments from "../Text/TextAddedComments.jsx";
-import BadgeAddedComments from "../Badge/BadgeAddedComments.jsx";
-import RadioDropdown from "../Inputs/RadioDropdown.jsx";
-
-export default function AccordionRatingComments() {
-
-
-  return (
-    <div id="rating-review-component" className="rating-div">
-      <form>
-        <RadioStarRating />
-        <TextAreaComment />
-      </form>
-      <BadgeAddedComments />
-      <RadioDropdown/>
-      Mapping 'added' comments from array to show them dynamically 
-      {commentsData.map((comment, index) => (
-        <TextAddedComments key={index} {...comment} />
-      ))}
-    </div>
-  );
-} */
-
-//test data comments
-
-/*  const commentsData = [
-    {
-      userName: "Heniox",
-      rating: 5,
-      dateAdded: "12.02.2024",
-      commentText: "super album !! PONADCZESOWEEEEEEE",
-    },
-    {
-      userName: "andrewww22",
-      rating: 4,
-      dateAdded: "21.01.2024",
-      commentText: "Sounds good, dope trackzz",
-    },
-    {
-      userName: "mia35",
-      rating: 5,
-      dateAdded: "07.11.2023",
-      commentText:
-        "I thought it would be crap but its underrated! I love this album",
-    },
-    {
-      userName: "mainhater111",
-      rating: 1,
-      dateAdded: "22.06.2023",
-      commentText:
-        "Wczoraj wieczorem, spacerując po malowniczej dzielnicy miasta, napotkałem niezwykłą kawiarnię z przytulnym wystrojem i aromatem świeżo mielonej kawy unoszącym się w powietrzu, co sprawiło, że chwilę zatrzymałem się, by zanurzyć w tej magicznej atmosferze.",
-    },
-  ]; */
 /* eslint-disable react/prop-types */
- import RadioStarRating from "../Inputs/RadioStarRating.jsx";
+/*  import RadioStarRating from "../Inputs/RadioStarRating.jsx";
 import TextAreaComment from "../Text/TextAreaComment.jsx";
 import TextAddedComments from "../Text/TextAddedComments.jsx";
 import BadgeAddedComments from "../Badge/BadgeAddedComments.jsx";
@@ -76,4 +20,49 @@ export default function AccordionRatingComments({ commentsData }) {
       ))}
     </div>
   );
-} 
+}  */
+/* eslint-disable react/prop-types */
+
+/* eslint-disable react/prop-types */
+
+import RadioStarRating from "../Inputs/RadioStarRating.jsx";
+import TextAreaComment from "../Text/TextAreaComment.jsx";
+import TextAddedComments from "../Text/TextAddedComments.jsx";
+import BadgeAddedComments from "../Badge/BadgeAddedComments.jsx";
+import RadioDropdown from "../Inputs/RadioDropdown.jsx";
+
+export default function AccordionRatingComments({ commentsData }) {
+  // Check if commentsData is defined and not null
+  if (!commentsData) {
+    return (
+      <div>
+        <div><p className="p-no-comments">Unusually quiet... Add the first comment!</p></div>
+        <form>
+          <RadioStarRating />
+          <TextAreaComment />
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <div id="rating-review-component" className="rating-div">
+      <form>
+        <RadioStarRating />
+        <TextAreaComment />
+      </form>
+      <BadgeAddedComments />
+      <RadioDropdown />
+
+      {/* Check if commentsData is empty */}
+      {commentsData.length === 0 ? (
+        <p>No comments yet</p>
+      ) : (
+        // If commentsData is not empty, map over it and display TextAddedComments
+        commentsData.map((comment, index) => (
+          <TextAddedComments key={index} {...comment} />
+        ))
+      )}
+    </div>
+  );
+}
--- src/UI/Badge/BadgeRatingProductInfo.jsx
+++ src/UI/Badge/BadgeRatingProductInfo.jsx
@@ -1,10 +1,70 @@
 /* eslint-disable react/prop-types */
 
-import IconTinyStarFilled from "../Icons/IconTinyStarFilled";
+/* import IconTinyStarFilled from "../Icons/IconTinyStarFilled";
 import "./BadgeRatingProductInfo.scss";
 
 export default function BadgeRatingProductInfo({ commentsData }) {
   //overallRating asa average of comments
+  const overallRating = calculateOverallRating(commentsData);
+
+  //counting how many reviews there are
+  const numberOfReviews = commentsData.length;
+
+  // counting average of comments
+  function calculateOverallRating(comments) {
+    if (comments.length === 0) return 0;
+
+    const totalRating = comments.reduce(
+      (sum, comment) => sum + comment.rating,
+      0
+    );
+    return totalRating / comments.length;
+  }
+
+  //scroll to accordion with reviews
+  const scrollToRatingReviews = () => {
+    const ratingReviews = document.getElementById("rating-review-component");
+    if (ratingReviews) {
+      ratingReviews.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="div-ratings">
+      <div className="rating-with-icon">
+        <p className="overall-number">{overallRating.toFixed(1)}</p>
+        <IconTinyStarFilled />
+         button to trigger the scroll to reviews component
+        <button
+          type="button"
+          className="number-of-reviews"
+          onClick={scrollToRatingReviews}
+        >
+          {numberOfReviews} reviews
+        </button>
+      </div>
+    </div>
+  );
+} */
+
+/* eslint-disable react/prop-types */
+
+/* eslint-disable react/prop-types */
+
+import IconTinyStarFilled from "../Icons/IconTinyStarFilled";
+import "./BadgeRatingProductInfo.scss";
+
+export default function BadgeRatingProductInfo({ commentsData }) {
+  // Check if commentsData is defined and not null
+  if (!commentsData) {
+    return (
+      <>
+        <p> No reviews yet.</p>
+      </>
+    );
+  }
+
+  //overallRating as average of comments
   const overallRating = calculateOverallRating(commentsData);
 
   //counting how many reviews there are

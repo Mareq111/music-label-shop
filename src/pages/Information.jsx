fe61c0a5a7643117c65db818adac010f6f3f9a0c
marq sz <marekszulc2020@gmail.com>
2024-06-10 09:39:40 +0200

    feat: add first content section into information page

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import "./Information.scss";
+import "./SubpagesInfoShoppingAuthorAccount.scss";
 export default function Information() {
   const location = useLocation();
   const margin = 32; // 32px = 2rem
@@ -22,18 +23,52 @@ export default function Information() {
 
   return (
     <div className="subpage">
-      <div className="head-of-subpage">Information</div>
-      <div className="content-div-subpage" id="information-contact">
-        Contact
+      <div className="head-of-subpage">
+        <h3 className="h-text-subpage">Information</h3>
+        <p className="p-subpage-description">
+          Explore essential details about our operations here. You'll find
+          contact information, our story, frequently asked questions, and our
+          privacy policy.
+        </p>
       </div>
-      <div className="content-div-subpage" id="information-about-us">
-        About Us
-      </div>
-      <div className="content-div-subpage" id="information-faq">
-        FAQ
-      </div>
-      <div className="content-div-subpage" id="information-privacy-policy">
-        Privacy Policy
+
+      {/* all sections into page */}
+      <div className="all-sections-subpage">
+        <section className="content-section-subpage" id="information-contact">
+          <div className="inside-content-section-subpage">
+            <h4 className="h-inside-content-section-subpage">Contact</h4>
+            <div className="content-into-section">
+              <div className="div-h5-p-into-subpage">
+                <h5 className="h5-">Email: </h5>
+                <p className="p-into-subpage">
+                  strimzempire.info@strimzempire.com
+                </p>
+              </div>
+              <div className="div-h5-p-into-subpage">
+                <h5 className="h5-into-subpage">Phone:</h5>
+                <p className="p-into-subpage">+1234567890</p>
+              </div>
+
+              <div className="div-h5-p-into-subpage">
+                <h5 className="h5-into-subpage"> Address: </h5>
+                <p className="p-into-subpage">123 Main Street, City, Country</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="content-section-subpage" id="information-about-us">
+          About Us
+        </section>
+        <section className="content-section-subpage" id="information-faq">
+          FAQ
+        </section>
+        <section
+          className="content-section-subpage"
+          id="information-privacy-policy"
+        >
+          Privacy Policy
+        </section>
       </div>
     </div>
   );

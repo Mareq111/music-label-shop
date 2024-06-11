a6477b2a1bcd9b19c437271a10a69d49647ec8d0
marq sz <marekszulc2020@gmail.com>
2024-06-11 08:35:01 +0200

    feat: add about us section into information page

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import "./SubpagesInfoShoppingAuthorAccount.scss";
+import BtnCheckWebsite from "../UI/Buttons/BtnCheckWebsite";
 export default function Information() {
   const location = useLocation();
   const margin = 32; // 32px = 2rem
@@ -34,31 +35,54 @@ export default function Information() {
 
       {/* all sections into page */}
       <div className="all-sections-subpage">
+        {/* section contact */}
+
         <section className="content-section-subpage" id="information-contact">
           <div className="inside-content-section-subpage">
             <h4 className="h-inside-content-section-subpage">Contact</h4>
             <div className="content-into-section">
               <div className="div-h5-p-into-subpage">
-                <h5 className="h5-">Email: </h5>
-                <p className="p-into-subpage">
-                  strimzempire.info@strimzempire.com
-                </p>
+                <h5 className="h5-into-subpage ">Email: </h5>
+                <p className="p-into-subpage">office@strimz-empire.com</p>
               </div>
               <div className="div-h5-p-into-subpage">
                 <h5 className="h5-into-subpage">Phone:</h5>
-                <p className="p-into-subpage">+1234567890</p>
+                <p className="p-into-subpage">+48 133 552 7559</p>
               </div>
 
               <div className="div-h5-p-into-subpage">
                 <h5 className="h5-into-subpage"> Address: </h5>
-                <p className="p-into-subpage">123 Main Street, City, Country</p>
+                <p className="p-into-subpage"> Future Street 13, Warsaw</p>
               </div>
             </div>
           </div>
         </section>
-
+        {/* section about us */}
         <section className="content-section-subpage" id="information-about-us">
-          About Us
+          <div className="inside-content-section-subpage">
+            <h4 className="h-inside-content-section-subpage">About Us</h4>
+            <div className="content-into-section">
+              <div className="div-h5-p-into-subpage">
+                <h5 className="h5-into-subpage">Our store</h5>
+                <p className="p-into-subpage">
+                  We are the official online store of Strimz Empire music label,
+                  offering albums and exclusive merchandise from our talented
+                  artists. Explore our collection to find the latest music
+                  releases, apparel, accessories, and more, all crafted with the
+                  unique style and quality of Strimz Empire
+                </p>
+              </div>
+              <div className="div-h5-p-into-subpage">
+                <h5 className="h5-into-subpage">See full story </h5>
+                <p className="p-into-subpage">
+                  Our website, for more check about' tab.
+                </p>
+                <div className="btn-checkwebsite-div">
+                  <BtnCheckWebsite />
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
         <section className="content-section-subpage" id="information-faq">
           FAQ

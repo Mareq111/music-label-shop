ae7c085decdd6a26ffaea51485727e947b0f6c0e
marq sz <marekszulc2020@gmail.com>
2024-06-13 09:39:32 +0200

    feat: add clear cart state after payment greetings

--- src/pages/PaymentAfterGreetings.jsx
+++ src/pages/PaymentAfterGreetings.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+/* import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import BtnContinue from "../UI/Buttons/BtnContinue";
 import BadgeTopOfAsideNav from "../UI/Badge/BadgeTopOfAsideNav";
@@ -29,6 +29,60 @@ export default function PaymentAfterGreetings() {
           {showCloseButton && (
             <div>
               <Link className="btn-continue-div-wrapper" to={"/"}>
+                <BtnContinue continueBtnText={"Back to Home"} />
+              </Link>
+            </div>
+          )}
+        </div>
+      </aside>
+    </>
+  );
+}
+ */
+
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import BtnContinue from "../UI/Buttons/BtnContinue";
+import BadgeTopOfAsideNav from "../UI/Badge/BadgeTopOfAsideNav";
+import PaymentSuccess from "../components/PaymentSuccess";
+import { clearCart } from "../store/cartSlice";
+
+export default function PaymentAfterGreetings() {
+  const [showCloseButton, setShowCloseButton] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowCloseButton(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  //reset cart after payment
+  const handleBackToHome = () => {
+    dispatch(clearCart());
+  };
+
+  return (
+    <>
+      <div className="backdrop"></div>
+      <aside className="aside-cart">
+        <BadgeTopOfAsideNav
+          nameOfNavPage={"Order summary "}
+          showCloseButton={false}
+        />
+        <hr className="cart-devider-separator" />
+        <div className="content-fill-cart">
+          <PaymentSuccess />
+          {showCloseButton && (
+            <div>
+              <Link
+                className="btn-continue-div-wrapper"
+                to={"/"}
+                onClick={handleBackToHome}
+              >
                 <BtnContinue continueBtnText={"Back to Home"} />
               </Link>
             </div>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -15,72 +15,6 @@ const cartSlice = createSlice({
       );
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.key,
-          price: newItem.price,
-          quantity: 1,
-          totalPrice: newItem.price,
-          title:
-            newItem.titleArtist ||
-            newItem.location ||
-            newItem.color ||
-            newItem.itemTitle ||
-            newItem.itemLevel,
-          imgURL: newItem.imgURL,
-          productId: newItem.productId,
-          priceItem: newItem.priceItem,
-          titleItem: newItem.titleItem,
-          selectedVersion: newItem.selectedVersion,
-        });
-      } else {
-        existingItem.quantity++;
-        existingItem.totalPrice += newItem.price;
-      }
-      state.totalQuantity++;
-    },
-    removeItemFromCart(state, action) {
-      const itemIdToRemove = action.payload;
-      const existingItem = state.items.find(
-        (item) => item.itemId === itemIdToRemove
-      );
-      if (!existingItem) {
-        return;
-      }
-      if (existingItem.quantity === 1) {
-        state.items = state.items.filter(
-          (item) => item.itemId !== itemIdToRemove
-        );
-      } else {
-        existingItem.quantity--;
-        existingItem.totalPrice -= existingItem.price;
-      }
-      state.totalQuantity--;
-    },
-  },
-});
-
-export const { addItemToCart, removeItemFromCart } = cartSlice.actions;
-export default cartSlice.reducer; */
-
-
-
-
-import { createSlice } from "@reduxjs/toolkit";
-
-const cartSlice = createSlice({
-  name: "cart",
-  initialState: {
-    items: [],
-    totalQuantity: 0,
-    totalPrice: 0,
-  },
-  reducers: {
-    addItemToCart(state, action) {
-      const newItem = action.payload;
-      const existingItem = state.items.find(
-        (item) => item.itemId === newItem.itemId
-      );
-      if (!existingItem) {
-        state.items.push({
           ...newItem,
           quantity: 1,
           itemId: newItem.key,
@@ -136,4 +70,88 @@ const cartSlice = createSlice({
 
 export const { addItemToCart, removeItemFromCart, updateItemQuantity } =
   cartSlice.actions;
+export default cartSlice.reducer;
+ */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    totalPrice: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.itemId === newItem.itemId
+      );
+      if (!existingItem) {
+        state.items.push({
+          ...newItem,
+          quantity: 1,
+          itemId: newItem.key,
+          totalPrice: newItem.priceItem,
+          title:
+            newItem.titleArtist ||
+            newItem.location ||
+            newItem.color ||
+            newItem.itemTitle ||
+            newItem.itemLevel,
+          imgURL: newItem.imgURL,
+          productId: newItem.productId,
+          priceItem: newItem.priceItem,
+          titleItem: newItem.titleItem,
+          selectedVersion: newItem.selectedVersion,
+        });
+        state.totalQuantity += 1;
+        state.totalPrice += newItem.priceItem;
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.priceItem;
+        state.totalQuantity += 1;
+        state.totalPrice += newItem.priceItem;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const itemIdToRemove = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.itemId === itemIdToRemove
+      );
+      if (!existingItem) return;
+
+      state.totalQuantity -= existingItem.quantity;
+      state.totalPrice -= existingItem.totalPrice;
+
+      state.items = state.items.filter(
+        (item) => item.itemId !== itemIdToRemove
+      );
+    },
+    updateItemQuantity(state, action) {
+      const { itemId, quantity } = action.payload;
+      const existingItem = state.items.find((item) => item.itemId === itemId);
+      if (existingItem) {
+        const quantityDifference = quantity - existingItem.quantity;
+        existingItem.quantity = quantity;
+        existingItem.totalPrice = existingItem.priceItem * quantity;
+        state.totalQuantity += quantityDifference;
+        state.totalPrice += quantityDifference * existingItem.priceItem;
+      }
+    },
+    clearCart(state) {
+      state.items = [];
+      state.totalQuantity = 0;
+      state.totalPrice = 0;
+    },
+  },
+});
+
+export const {
+  addItemToCart,
+  removeItemFromCart,
+  updateItemQuantity,
+  clearCart,
+} = cartSlice.actions;
 export default cartSlice.reducer;

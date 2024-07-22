39cf5ce54149dc0cd921b4d3e5633caa8e5d9a9b
marq sz <marekszulc2020@gmail.com>
2024-07-22 08:07:51 +0200

    feat: make approach to input search

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -107,7 +107,7 @@ export default function SearchResults() {
 }
  */
 
-import { useEffect, useState } from "react";
+/* import { useEffect, useState } from "react";
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 import { useLocation } from "react-router-dom";
@@ -180,6 +180,330 @@ export default function SearchResults() {
         ) : (
           <p>No products found</p>
         )}
+        <BadgeCategoriesWithH />
+      </div>
+    </div>
+  );
+}
+ */
+/* 
+import { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import { Link, useLocation } from "react-router-dom";
+import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
+import "./SearchResult.scss";
+import firebaseConfig from "../firebaseConfig.js";
+import CardProductMain from "../UI/Cards/CardProductMain.jsx";
+
+// Initialize Firebase if it hasn't been initialized yet
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function SearchResults() {
+  const [searchResults, setSearchResults] = useState([]);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("q");
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (query) {
+        const categories = [
+          { name: "albums", index: "keywords" },
+          { name: "tickets", index: "country" },
+          { name: "posters", index: "keywords" },
+          { name: "t-shirts", index: "keywords" },
+          { name: "gadgets", index: "keywords" },
+          { name: "puzzles", index: "keywords" },
+        ];
+        let results = [];
+  
+        for (const category of categories) {
+          const ref = firebase
+            .database()
+            .ref(`categories/${category.name}/products`);
+          const snapshot = await ref
+            .orderByChild(category.index)
+            .startAt(query)
+            .endAt(query + "\uf8ff")
+            .once("value");
+  
+          const data = snapshot.val();
+          if (data) {
+            const categoryResults = Object.entries(data).map(
+              ([key, value]) => ({ key, ...value, category: category.name })
+            );
+            results = [...results, ...categoryResults];
+          }
+        }
+  
+        setSearchResults(results);
+      } else {
+        setSearchResults([]);
+      }
+    };
+  
+    fetchSearchResults();
+  }, [query]);
+  
+  
+  return (
+    <div className="all-search-page">
+      <div className="categories-search-div">
+       
+        <div className="div"></div>
+      </div>
+      <div className="search-results">
+        <ul>
+          {searchResults.map((item) => (
+            <li className="li-productsMain" key={item.key}>
+              <Link to={`/${item.key}`}>
+                <CardProductMain product={item} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <BadgeCategoriesWithH />
+      </div>
+    </div>
+  );
+}
+ */
+
+/* import { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import { Link, useLocation } from "react-router-dom";
+import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
+import "./SearchResult.scss";
+import firebaseConfig from "../firebaseConfig.js";
+import CardProductMain from "../UI/Cards/CardProductMain.jsx";
+
+// Initialize Firebase if it hasn't been initialized yet
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function SearchResults() {
+  const [searchResults, setSearchResults] = useState([]);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("q");
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (query) {
+        const categories = [
+          { name: "albums", index: "keywords" },
+          { name: "tickets", index: "country" },
+          { name: "posters", index: "keywords" },
+          { name: "t-shirts", index: "keywords" },
+          { name: "gadgets", index: "keywords" },
+          { name: "puzzles", index: "keywords" },
+        ];
+        let results = [];
+
+        for (const category of categories) {
+          const ref = firebase.database().ref(`categories/${category.name}/products`);
+          const snapshot = await ref.orderByChild(category.index).startAt(query).endAt(query + "\uf8ff").once("value");
+
+          const data = snapshot.val();
+          if (data) {
+            const categoryResults = Object.entries(data).map(([key, value]) => ({ key, ...value, category: category.name }));
+            results = [...results, ...categoryResults];
+          }
+        }
+
+        setSearchResults(results);
+      } else {
+        setSearchResults([]);
+      }
+    };
+
+    fetchSearchResults();
+  }, [query]);
+
+  return (
+    <div className="all-search-page">
+      <div className="categories-search-div">
+        <div className="div"></div>
+      </div>
+      <div className="search-results">
+        <ul>
+          {searchResults.map((item) => (
+            <li className="li-productsMain" key={item.key}>
+              <Link to={`/${item.key}`}>
+                <CardProductMain product={item} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <BadgeCategoriesWithH />
+      </div>
+    </div>
+  );
+}
+ */
+
+/* import { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import { Link, useLocation } from "react-router-dom";
+import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
+import "./SearchResult.scss";
+import firebaseConfig from "../firebaseConfig.js";
+import CardProductMain from "../UI/Cards/CardProductMain.jsx";
+
+// Initialize Firebase if it hasn't been initialized yet
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function SearchResults() {
+  const [searchResults, setSearchResults] = useState([]);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("q");
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (query) {
+        const categories = [
+          { name: "albums", index: "keywords" },
+          { name: "tickets", index: "country" },
+          { name: "posters", index: "keywords" },
+          { name: "t-shirts", index: "keywords" },
+          { name: "gadgets", index: "keywords" },
+          { name: "puzzles", index: "keywords" },
+        ];
+        let results = [];
+
+        for (const category of categories) {
+          const ref = firebase.database().ref(`categories/${category.name}/products`);
+          const snapshot = await ref.once("value");
+          const data = snapshot.val();
+
+          if (data) {
+            const categoryResults = Object.entries(data)
+              .map(([key, value]) => ({ key, ...value, category: category.name }))
+              .filter((product) => {
+                const keywordsString = product.keywords.join(" ").toLowerCase();
+                return keywordsString.includes(query.toLowerCase());
+              });
+            results = [...results, ...categoryResults];
+          }
+        }
+
+        setSearchResults(results);
+      } else {
+        setSearchResults([]);
+      }
+    };
+
+    fetchSearchResults();
+  }, [query]);
+
+  return (
+    <div className="all-search-page">
+      <div className="categories-search-div">
+        <div className="div"></div>
+      </div>
+      <div className="search-results">
+        <ul>
+          {searchResults.map((item) => (
+            <li className="li-productsMain" key={item.key}>
+              <Link to={`/${item.key}`}>
+                <CardProductMain product={item} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <BadgeCategoriesWithH />
+      </div>
+    </div>
+  );
+}
+ */
+
+import { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import { Link, useLocation } from "react-router-dom";
+import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
+import "./SearchResult.scss";
+import firebaseConfig from "../firebaseConfig.js";
+import CardProductMain from "../UI/Cards/CardProductMain.jsx";
+
+// Initialize Firebase if it hasn't been initialized yet
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function SearchResults() {
+  const [searchResults, setSearchResults] = useState([]);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("q");
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (query) {
+        const categories = [
+          { name: "albums", index: "keywords" },
+          { name: "tickets", index: "country" },
+          { name: "posters", index: "keywords" },
+          { name: "t-shirts", index: "keywords" },
+          { name: "gadgets", index: "keywords" },
+          { name: "puzzles", index: "keywords" },
+        ];
+        let results = [];
+
+        for (const category of categories) {
+          const ref = firebase
+            .database()
+            .ref(`categories/${category.name}/products`);
+          const snapshot = await ref.once("value");
+          const data = snapshot.val();
+
+          if (data) {
+            const categoryResults = Object.entries(data)
+              .map(([key, value]) => ({
+                key,
+                ...value,
+                category: category.name,
+              }))
+              .filter((product) => {
+                const keywordsString = Array.isArray(product.keywords)
+                  ? product.keywords.join(" ").toLowerCase()
+                  : "";
+                return keywordsString.includes(query.toLowerCase());
+              });
+            results = [...results, ...categoryResults];
+          }
+        }
+
+        setSearchResults(results);
+      } else {
+        setSearchResults([]);
+      }
+    };
+
+    fetchSearchResults();
+  }, [query]);
+
+  return (
+    <div className="all-search-page">
+      <div className="categories-search-div">
+        <div className="div"></div>
+      </div>
+      <div className="search-results">
+        <ul>
+          {searchResults.map((item) => (
+            <li className="li-productsMain" key={item.key}>
+              <Link to={`/${item.key}`}>
+                <CardProductMain product={item} />
+              </Link>
+            </li>
+          ))}
+        </ul>
         <BadgeCategoriesWithH />
       </div>
     </div>

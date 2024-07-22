0ea1670de2efd7966ce3f5a0d43a7e26cdcfbd1a
marq sz <marekszulc2020@gmail.com>
2024-07-22 08:22:29 +0200

    feat: add correct rendering searching product initially

--- src/UI/Cards/CardProductMain.jsx
+++ src/UI/Cards/CardProductMain.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import "./CardProductMain.scss";
+/* import "./CardProductMain.scss";
 
 export default function CardProductMain({ product, layout, customClass }) {
   //!descruturing the product object of location or color or itemTitle or itemLevel if its not an album which has titleArtist
@@ -74,5 +74,89 @@ export default function CardProductMain({ product, layout, customClass }) {
 
   return (
     <div className="card-product-main-container">{renderCardContent()}</div>
+  );
+}
+ */
+
+
+
+import "./CardProductMain.scss";
+
+export default function CardProductMain({ product, layout, customClass }) {
+  console.log("CardProductMain props:", { product, layout, customClass }); // Add console log
+
+  //! Destructuring the product object of location or color or itemTitle or itemLevel if it's not an album which has titleArtist
+  const {
+    titleItem,
+    titleArtist = product.location ||
+      product.color ||
+      product.itemTitle ||
+      product.itemLevel,
+    priceItem,
+    imgURL,
+  } = product;
+
+  const renderCardContent = () => {
+    if (layout === "grid") {
+      return (
+        <div className={`card__Product-list ${customClass}`}>
+          <div className="div-top-of-card-Product-list">
+            <div className="div-img-card__Product-list">
+              <img
+                className="product-image-list"
+                src={imgURL}
+                alt={`${titleItem} by ${titleArtist}`}
+              />
+            </div>
+          </div>
+          <div className="div-bottom-of-card-Product-list">
+            <div className="two-titles-card-Product-list">
+              <div className="div-title-card__Product-list">
+                <h4 className="title-card__Product-list">{titleItem}</h4>
+              </div>
+              <div className="div-title-card__Product-list">
+                <h5 className="titleArtist-card__Product-list">
+                  {titleArtist}
+                </h5>
+              </div>
+            </div>
+            <h5 className="price-card__Product-list">From {priceItem}€</h5>
+          </div>
+        </div>
+      );
+    } else if (layout === "list") {
+      return (
+        <div className={`card__Product-grid ${customClass}`}>
+          <div className="div-top-of-card-Product-grid">
+            <div className="div-img-card__Product-grid">
+              <img
+                className="product-image-grid"
+                src={imgURL}
+                alt={`${titleItem} by ${titleArtist}`}
+              />
+            </div>
+          </div>
+          <div className="div-bottom-of-card-Product-grid">
+            <div className="two-titles-card-Product-grid">
+              <div className="div-title-card__Product-grid">
+                <h4 className="title-card__Product-grid">{titleItem}</h4>
+              </div>
+              <div className="div-title-card__Product-grid">
+                <h5 className="titleArtist-card__Product-grid">
+                  {titleArtist}
+                </h5>
+              </div>
+            </div>
+            <h5 className="price-card__Product-grid">From {priceItem}€</h5>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="card-product-main-container">
+      {renderCardContent()}
+    </div>
   );
 }
--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,429 +1,5 @@
 /* eslint-disable react/prop-types */
-
-/* import "./SearchResult.scss";
-import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
-export default function SearchResult({ searchResults }) {
-  return (
-    <div className="all-search-page">
-      <div className="categories-search-div">
-        <div className="div"></div>
-      </div>
-      <div className="search-results">
-        {searchResults.length > 0 ? (
-          searchResults.map((product, index) => (
-            <div key={index} className="search-result-item">
-              <p>Check out our results</p>
-              <h3>{product.titleArtist || product.name}</h3>
-              <p>{product.TitleItem}</p>
-            </div>
-          ))
-        ) : (
-          <p>No products found</p>
-        )}
-        <BadgeCategoriesWithH />
-      </div>
-    </div>
-  );
-}
- */
-
-/* import { useEffect, useState } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/database";
-import { useLocation } from "react-router-dom";
-import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
-import "./SearchResult.scss";
-import firebaseConfig from "../firebaseConfig.js";
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export default function SearchResults() {
-  const [searchResults, setSearchResults] = useState([]);
-  const location = useLocation();
-  const query = new URLSearchParams(location.search).get("q");
-
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      if (query) {
-        const categories = [
-          "albums",
-          "tickets",
-          "posters",
-          "t-shirts",
-          "gadgets",
-          "puzzles",
-        ];
-        let results = [];
-
-        for (const category of categories) {
-          const ref = firebase
-            .database()
-            .ref(`categories/${category}/products`);
-          const snapshot = await ref
-            .orderByChild("titleArtist")
-            .startAt(query)
-            .endAt(query + "\uf8ff")
-            .once("value");
-
-          const data = snapshot.val();
-          if (data) {
-            const categoryResults = Object.entries(data).map(
-              ([key, value]) => ({ key, ...value, category })
-            );
-            results = [...results, ...categoryResults];
-          }
-        }
-
-        setSearchResults(results);
-      }
-    };
-
-    fetchSearchResults();
-  }, [query]);
-
-  return (
-    <div className="all-search-page">
-      <div className="categories-search-div">
-        <div className="div"></div>
-      </div>
-      <div className="search-results">
-        {searchResults.length > 0 ? (
-          searchResults.map((product, index) => (
-            <div key={index} className="search-result-item">
-              <p>Check out our results</p>
-              <h3>{product.titleArtist || product.name}</h3>
-              <p>{product.TitleItem}</p>
-            </div>
-          ))
-        ) : (
-          <p>No products found</p>
-        )}
-        <BadgeCategoriesWithH />
-      </div>
-    </div>
-  );
-}
- */
-
-/* import { useEffect, useState } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/database";
-import { useLocation } from "react-router-dom";
-import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
-import "./SearchResult.scss";
-import firebaseConfig from "../firebaseConfig.js";
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export default function SearchResults() {
-  const [searchResults, setSearchResults] = useState([]);
-  const location = useLocation();
-  const query = new URLSearchParams(location.search).get("q");
-
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      if (query) {
-        const categories = [
-          { name: "albums", index: "titleArtist" },
-          { name: "tickets", index: "country" },
-          { name: "posters", index: "titleArtist" },
-          { name: "t-shirts", index: "color" },
-          { name: "gadgets", index: "itemTitle" },
-          { name: "puzzles", index: "itemLevel" },
-        ];
-        let results = [];
-
-        for (const category of categories) {
-          const ref = firebase
-            .database()
-            .ref(`categories/${category.name}/products`);
-          const snapshot = await ref
-            .orderByChild(category.index)
-            .startAt(query)
-            .endAt(query + "\uf8ff")
-            .once("value");
-
-          const data = snapshot.val();
-          if (data) {
-            const categoryResults = Object.entries(data).map(
-              ([key, value]) => ({ key, ...value, category: category.name })
-            );
-            results = [...results, ...categoryResults];
-          }
-        }
-
-        setSearchResults(results);
-      }
-    };
-
-    fetchSearchResults();
-  }, [query]);
-
-  return (
-    <div className="all-search-page">
-      <div className="categories-search-div">
-        <div className="div"></div>
-      </div>
-      <div className="search-results">
-        {searchResults.length > 0 ? (
-          searchResults.map((product, index) => (
-            <div key={index} className="search-result-item">
-              <p>Check out our results</p>
-              <h3>{product.titleArtist || product.name}</h3>
-              <p>{product.TitleItem}</p>
-            </div>
-          ))
-        ) : (
-          <p>No products found</p>
-        )}
-        <BadgeCategoriesWithH />
-      </div>
-    </div>
-  );
-}
- */
 /* 
-import { useEffect, useState } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/database";
-import { Link, useLocation } from "react-router-dom";
-import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
-import "./SearchResult.scss";
-import firebaseConfig from "../firebaseConfig.js";
-import CardProductMain from "../UI/Cards/CardProductMain.jsx";
-
-// Initialize Firebase if it hasn't been initialized yet
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export default function SearchResults() {
-  const [searchResults, setSearchResults] = useState([]);
-  const location = useLocation();
-  const query = new URLSearchParams(location.search).get("q");
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      if (query) {
-        const categories = [
-          { name: "albums", index: "keywords" },
-          { name: "tickets", index: "country" },
-          { name: "posters", index: "keywords" },
-          { name: "t-shirts", index: "keywords" },
-          { name: "gadgets", index: "keywords" },
-          { name: "puzzles", index: "keywords" },
-        ];
-        let results = [];
-  
-        for (const category of categories) {
-          const ref = firebase
-            .database()
-            .ref(`categories/${category.name}/products`);
-          const snapshot = await ref
-            .orderByChild(category.index)
-            .startAt(query)
-            .endAt(query + "\uf8ff")
-            .once("value");
-  
-          const data = snapshot.val();
-          if (data) {
-            const categoryResults = Object.entries(data).map(
-              ([key, value]) => ({ key, ...value, category: category.name })
-            );
-            results = [...results, ...categoryResults];
-          }
-        }
-  
-        setSearchResults(results);
-      } else {
-        setSearchResults([]);
-      }
-    };
-  
-    fetchSearchResults();
-  }, [query]);
-  
-  
-  return (
-    <div className="all-search-page">
-      <div className="categories-search-div">
-       
-        <div className="div"></div>
-      </div>
-      <div className="search-results">
-        <ul>
-          {searchResults.map((item) => (
-            <li className="li-productsMain" key={item.key}>
-              <Link to={`/${item.key}`}>
-                <CardProductMain product={item} />
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <BadgeCategoriesWithH />
-      </div>
-    </div>
-  );
-}
- */
-
-/* import { useEffect, useState } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/database";
-import { Link, useLocation } from "react-router-dom";
-import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
-import "./SearchResult.scss";
-import firebaseConfig from "../firebaseConfig.js";
-import CardProductMain from "../UI/Cards/CardProductMain.jsx";
-
-// Initialize Firebase if it hasn't been initialized yet
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export default function SearchResults() {
-  const [searchResults, setSearchResults] = useState([]);
-  const location = useLocation();
-  const query = new URLSearchParams(location.search).get("q");
-
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      if (query) {
-        const categories = [
-          { name: "albums", index: "keywords" },
-          { name: "tickets", index: "country" },
-          { name: "posters", index: "keywords" },
-          { name: "t-shirts", index: "keywords" },
-          { name: "gadgets", index: "keywords" },
-          { name: "puzzles", index: "keywords" },
-        ];
-        let results = [];
-
-        for (const category of categories) {
-          const ref = firebase.database().ref(`categories/${category.name}/products`);
-          const snapshot = await ref.orderByChild(category.index).startAt(query).endAt(query + "\uf8ff").once("value");
-
-          const data = snapshot.val();
-          if (data) {
-            const categoryResults = Object.entries(data).map(([key, value]) => ({ key, ...value, category: category.name }));
-            results = [...results, ...categoryResults];
-          }
-        }
-
-        setSearchResults(results);
-      } else {
-        setSearchResults([]);
-      }
-    };
-
-    fetchSearchResults();
-  }, [query]);
-
-  return (
-    <div className="all-search-page">
-      <div className="categories-search-div">
-        <div className="div"></div>
-      </div>
-      <div className="search-results">
-        <ul>
-          {searchResults.map((item) => (
-            <li className="li-productsMain" key={item.key}>
-              <Link to={`/${item.key}`}>
-                <CardProductMain product={item} />
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <BadgeCategoriesWithH />
-      </div>
-    </div>
-  );
-}
- */
-
-/* import { useEffect, useState } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/database";
-import { Link, useLocation } from "react-router-dom";
-import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
-import "./SearchResult.scss";
-import firebaseConfig from "../firebaseConfig.js";
-import CardProductMain from "../UI/Cards/CardProductMain.jsx";
-
-// Initialize Firebase if it hasn't been initialized yet
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export default function SearchResults() {
-  const [searchResults, setSearchResults] = useState([]);
-  const location = useLocation();
-  const query = new URLSearchParams(location.search).get("q");
-
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      if (query) {
-        const categories = [
-          { name: "albums", index: "keywords" },
-          { name: "tickets", index: "country" },
-          { name: "posters", index: "keywords" },
-          { name: "t-shirts", index: "keywords" },
-          { name: "gadgets", index: "keywords" },
-          { name: "puzzles", index: "keywords" },
-        ];
-        let results = [];
-
-        for (const category of categories) {
-          const ref = firebase.database().ref(`categories/${category.name}/products`);
-          const snapshot = await ref.once("value");
-          const data = snapshot.val();
-
-          if (data) {
-            const categoryResults = Object.entries(data)
-              .map(([key, value]) => ({ key, ...value, category: category.name }))
-              .filter((product) => {
-                const keywordsString = product.keywords.join(" ").toLowerCase();
-                return keywordsString.includes(query.toLowerCase());
-              });
-            results = [...results, ...categoryResults];
-          }
-        }
-
-        setSearchResults(results);
-      } else {
-        setSearchResults([]);
-      }
-    };
-
-    fetchSearchResults();
-  }, [query]);
-
-  return (
-    <div className="all-search-page">
-      <div className="categories-search-div">
-        <div className="div"></div>
-      </div>
-      <div className="search-results">
-        <ul>
-          {searchResults.map((item) => (
-            <li className="li-productsMain" key={item.key}>
-              <Link to={`/${item.key}`}>
-                <CardProductMain product={item} />
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <BadgeCategoriesWithH />
-      </div>
-    </div>
-  );
-}
- */
-
 import { useEffect, useState } from "react";
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
@@ -503,6 +79,187 @@ export default function SearchResults() {
               </Link>
             </li>
           ))}
+        </ul>
+        <BadgeCategoriesWithH />
+      </div>
+    </div>
+  );
+}
+ */
+
+/* import { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import { Link, useLocation } from "react-router-dom";
+import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
+import "./SearchResult.scss";
+import firebaseConfig from "../firebaseConfig.js";
+import CardProductMain from "../UI/Cards/CardProductMain.jsx";
+
+// Initialize Firebase if it hasn't been initialized yet
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function SearchResults() {
+  const [searchResults, setSearchResults] = useState([]);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("q");
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (query) {
+        const categories = [
+          { name: "albums", index: "keywords" },
+          { name: "tickets", index: "country" },
+          { name: "posters", index: "keywords" },
+          { name: "t-shirts", index: "keywords" },
+          { name: "gadgets", index: "keywords" },
+          { name: "puzzles", index: "keywords" },
+        ];
+        let results = [];
+
+        for (const category of categories) {
+          const ref = firebase.database().ref(`categories/${category.name}/products`);
+          const snapshot = await ref.once("value");
+          const data = snapshot.val();
+
+          if (data) {
+            const categoryResults = Object.entries(data)
+              .map(([key, value]) => ({ key, ...value, category: category.name }))
+              .filter((product) => {
+                const keywordsString = Array.isArray(product.keywords) ? product.keywords.join(" ").toLowerCase() : "";
+                return keywordsString.includes(query.toLowerCase());
+              });
+            results = [...results, ...categoryResults];
+          }
+        }
+
+        console.log("Fetched search results:", results); // Add debug log
+        setSearchResults(results);
+      } else {
+        setSearchResults([]);
+      }
+    };
+
+    fetchSearchResults();
+  }, [query]);
+
+  return (
+    <div className="all-search-page">
+      <div className="categories-search-div">
+        <div className="div"></div>
+      </div>
+      <div className="search-results">
+        <ul>
+          {searchResults.map((item) => {
+            console.log("Rendering item:", item); 
+            return (
+              <li className="li-productsMain" key={item.key}>
+                <Link to={`/${item.key}`}>
+                  <CardProductMain product={item} />
+                </Link>
+              </li>
+           
+            );
+          })}
+        </ul>
+        <BadgeCategoriesWithH />
+      
+      </div>
+    </div>
+  );
+}
+ */
+
+import { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import { Link, useLocation } from "react-router-dom";
+import BadgeCategoriesWithH from "../UI/Badge/BadgeCategoriesWithH";
+import "./SearchResult.scss";
+import firebaseConfig from "../firebaseConfig.js";
+import CardProductMain from "../UI/Cards/CardProductMain.jsx";
+
+// Initialize Firebase if it hasn't been initialized yet
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function SearchResults() {
+  const [searchResults, setSearchResults] = useState([]);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("q");
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (query) {
+        const categories = [
+          { name: "albums", index: "keywords" },
+          { name: "tickets", index: "country" },
+          { name: "posters", index: "keywords" },
+          { name: "t-shirts", index: "keywords" },
+          { name: "gadgets", index: "keywords" },
+          { name: "puzzles", index: "keywords" },
+        ];
+        let results = [];
+
+        for (const category of categories) {
+          const ref = firebase
+            .database()
+            .ref(`categories/${category.name}/products`);
+          const snapshot = await ref.once("value");
+          const data = snapshot.val();
+
+          if (data) {
+            const categoryResults = Object.entries(data)
+              .map(([key, value]) => ({
+                key,
+                ...value,
+                category: category.name,
+              }))
+              .filter((product) => {
+                const keywordsString = Array.isArray(product.keywords)
+                  ? product.keywords.join(" ").toLowerCase()
+                  : "";
+                return keywordsString.includes(query.toLowerCase());
+              });
+            results = [...results, ...categoryResults];
+          }
+        }
+
+        console.log("Fetched search results:", results); // Add debug log
+        setSearchResults(results);
+      } else {
+        setSearchResults([]);
+      }
+    };
+
+    fetchSearchResults();
+  }, [query]);
+
+  return (
+    <div className="all-search-page">
+      <div className="categories-search-div">
+        <div className="div"></div>
+      </div>
+      <div className="search-results">
+        <ul>
+          {searchResults.map((item) => {
+            console.log("Rendering item:", item); // Add this line
+            return (
+              <li className="li-productsMain" key={item.key}>
+                <Link to={`/${item.key}`}>
+                  <CardProductMain
+                    product={item}
+                    layout="grid"
+                    customClass=""
+                  />{" "}
+                  {/* Pass props */}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
         <BadgeCategoriesWithH />
       </div>

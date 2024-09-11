9e094abd0a71df88a70d46fd889e8d871bf0b0e9
marq sz <marekszulc2020@gmail.com>
2024-09-11 10:14:40 +0200

    feat: add all my coupons componetnt

--- src/pages/MyCoupons.jsx
+++ src/pages/MyCoupons.jsx
@@ -1,3 +1,56 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import "./SubpagesInfoShoppingAuthorAccount.scss";
 export default function MyCoupons() {
-  return <div>MyCoupons</div>;
+  const location = useLocation();
+  const margin = 32; // 32px = 2rem
+
+  useEffect(() => {
+    const hash = location.hash;
+    if (hash) {
+      const element = document.getElementById(hash.substring(1));
+      if (element) {
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.scrollY - margin;
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth",
+        });
+      }
+    }
+  }, [location]);
+
+  return (
+    <div className="subpage">
+      <div className="head-of-subpage">
+        <h3 className="h-text-subpage">My Coupons</h3>
+        <p className="p-subpage-description">
+          View and manage your available coupons. Save on your next purchase by
+          applying them at checkout.
+        </p>
+      </div>
+
+      {/* all sections into page */}
+      <div className="all-sections-subpage">
+        {/* section contact */}
+
+        <section className="content-section-subpage" id="return-policy">
+          <div className="inside-content-section-subpage">
+            <h4 className="h-inside-content-section-subpage">
+              Please log in to view your available coupons.
+            </h4>
+            <div className="content-into-section">
+              <div className="div-h5-p-into-subpage">
+                <h5 className="h5-into-subpage ">My Coupons</h5>
+                <p className="p-into-subpage">
+                  You currently have no available coupons. Follow us on social
+                  media to stay updated on the latest offers and discounts!
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }

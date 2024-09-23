68443018090f70de30160fa6ee03ffd0a0c7556b
marq sz <marekszulc2020@gmail.com>
2024-09-23 09:25:25 +0200

    refactor: add correct accessibility to chooser into details product

--- src/UI/Inputs/RadioInput.jsx
+++ src/UI/Inputs/RadioInput.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-
+/* 
 import "./RadioInput.scss";
 export default function RadioInput({
   radioLabel,
@@ -32,5 +32,54 @@ export default function RadioInput({
         </span>
       </label>
     </>
+  );
+}
+ */
+
+import "./RadioInput.scss";
+import { useRef } from "react";
+
+export default function RadioInput({
+  radioLabel,
+  radioId,
+  radioValue,
+  radioAriaLabel,
+  radioKey,
+  onChange,
+  isChecked,
+}) {
+  const radioRef = useRef(null);
+
+  const handleChange = (event) => {
+    onChange(event);
+  };
+
+  //Focus handling with Enter
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      //SIMULATE CLICK on input
+      radioRef.current.click();
+    }
+  };
+
+  return (
+    <label className="focus-label-input-tile" htmlFor={radioId}>
+      <input
+        ref={radioRef}
+        className="radio-input"
+        type="radio"
+        name="engine"
+        id={radioId}
+        value={`${radioValue} version`}
+        aria-label={radioAriaLabel}
+        key={radioKey}
+        onChange={handleChange}
+        checked={isChecked}
+      />
+      <span className="radio-tile" tabIndex={0} onKeyDown={handleKeyDown}>
+        <span className="radio-label">{radioLabel}</span>
+      </span>
+    </label>
   );
 }

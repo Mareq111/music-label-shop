c4ec2ad4fbbd91c3a23805bd0b8da688a168ca65
marq sz <marekszulc2020@gmail.com>
2024-10-01 08:12:22 +0200

    feat: add img posters for card info homepage initially

--- src/UI/Cards/CardInfoHomepage.jsx
+++ src/UI/Cards/CardInfoHomepage.jsx
@@ -1,10 +1,20 @@
 import "./CardInfoHomepage.scss";
+import setElDM from "../../assets/img/others/set-eldm.jpg";
+import setElDMTWO from "../../assets/img/others/set2-eldm.jpg";
+import BtnContinue from "../Buttons/BtnContinue";
 
 export default function CardInfoHomepage() {
   return (
     <article className="card-info-homepage" tabIndex={0}>
-      <div className="div-card-info-homepage">
+      {/*  <div className="div-card-info-homepage">
         <h3 className="h-card-info-homepage">TEST INFO</h3>
+      </div> */}
+      <div className="div-card-info-homepage">
+        <img src={setElDM} alt="" />
+        {/*    <h3 className="h-card-info-homepage">TEST INFO</h3> */}
+        <div className="wrap-btn-info-homepage">
+          <BtnContinue continueBtnText={"View 3 Posters Set"} />
+        </div>
       </div>
     </article>
   );

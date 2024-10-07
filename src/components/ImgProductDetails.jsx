3cab557c352b6ce78ca4337e8796f03f8dadaaec
marq sz <marekszulc2020@gmail.com>
2024-10-07 08:48:28 +0200

    refactor: add dependencies that third img is render if it available into imgrproduct details

--- src/components/ImgProductDetails.jsx
+++ src/components/ImgProductDetails.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-import "./ImgProductDetails.scss";
+/* import "./ImgProductDetails.scss";
 import MainImgProdDetails from "./../UI/Images/MainImgProdDetails";
 import SecondImgProdDetails from "./../UI/Images/SecondImgProdDetails";
 import BtnHeart from "../UI/Buttons/BtnHeart";
@@ -21,6 +21,35 @@ export default function ImgProductDetails({ productData }) {
         <div className="div-img-bottom">
           <SecondImgProdDetails imgSecondURL={productData.imgSecondURL} />
           <ThirdImgProdDetails imgThirdURL={productData.imgThirdURL} />
+        </div>
+      </div>
+    </div>
+  );
+} */
+import "./ImgProductDetails.scss";
+import MainImgProdDetails from "./../UI/Images/MainImgProdDetails";
+import SecondImgProdDetails from "./../UI/Images/SecondImgProdDetails";
+import BtnHeart from "../UI/Buttons/BtnHeart";
+import ThirdImgProdDetails from "../UI/Images/ThirdImgProdDetails";
+
+export default function ImgProductDetails({ productData }) {
+  console.log("Product Data:", productData);
+
+  return (
+    <div className="div-img-all">
+      <div className="div-img-main">
+        <MainImgProdDetails imgURL={productData.imgURL} />
+      </div>
+      <div className="badge">
+        <BtnHeart product={productData} />
+      </div>
+      <div className="div-img-bottom-all">
+        <div className="div-img-bottom">
+          <SecondImgProdDetails imgSecondURL={productData.imgSecondURL} />
+          {/* Render third component if 3 img are available  */}
+          {productData.imgThirdURL && (
+            <ThirdImgProdDetails imgThirdURL={productData.imgThirdURL} />
+          )}
         </div>
       </div>
     </div>
